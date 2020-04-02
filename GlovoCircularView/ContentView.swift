9b80398e36c7504c0958efea92de0d582bd1d31d
Amr Abdel-Wahab <amr@orcas.io>
2020-04-02 14:20:33 +0200

    Initial Commit

---
+++ GlovoCircularView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GlovoCircularView
+//
+//  Created by Amr AbdelWahab on 4/2/20.
+//  Copyright © 2020 Orcas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
