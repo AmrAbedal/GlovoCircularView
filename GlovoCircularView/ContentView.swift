f72a1c515d9c76f32cc0ca6568726f49af295604
Amr Abdel-Wahab <amr@orcas.io>
2020-04-03 19:49:17 +0200

    setup arc cell

--- GlovoCircularView/ContentView.swift
+++ GlovoCircularView/ContentView.swift
@@ -16,78 +16,102 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-//        Group {
             CustomView()
-//            CustomView().colorScheme(.dark)
-
-//        }
     }
 }
 
 struct CustomView: View {
-    var gradient1: Gradient {
-        return Gradient(colors: [.red,.green,.blue])
-    }
-    var gradient2: Gradient {
-        let stops =  [Gradient.Stop(color:.red,location: 0.1),
-                      Gradient.Stop(color:.green,location: 0.1),
-                      Gradient.Stop(color:.blue,location: 0.1),
-        ]
-        return Gradient(stops: stops)
-    }
+    let items = [Item(index: 1, name: "1",color: .red),
+                 Item(index: 2, name: "2",color:.green),
+                 Item(index: 3, name: "3",color:.yellow),
+                 Item(index: 4, name: "4",color:.blue),
+                 Item(index: 5, name: "5",color:.gray),
+                 Item(index: 6, name: "6",color: .black)]
    var body: some View {
-         VStack {
-            Circular(numofArcs: 5).stroke(style: .init(lineWidth: 10)).frame(width: 300, height: 300, alignment: .center).foregroundColor(.green).background(Circle().foregroundColor(.red))
-//            Heart().stroke()
-               }     }
-    
-
+    Circular2(items:items, arcCell: { item in
+        return ArcCell.init(name: item.name)
+    }
+ , width: 400)
+      }
 }
 
-struct Heart: Shape {
+struct Circular2: View {
+    let items: [Item]
+    let arcCell: (Item)->(ArcCell)
+    let width: Int
+    var raduis: CGFloat { return  CGFloat(width / 2) }
+    var angle: Int {return  360 / items.count }
+    var shift: Double {
+        return 0
+    }
+    var centerX: Int { return width / 2  }
+    var centerY: Int {return width / 2 }
+    var centerPoint: CGPoint { return CGPoint(x: centerX,y: centerY) }
+
+    var body: some View {
+        ZStack {
+            ForEach(items){ item in
+                self.getArcView(item: item)
+                
+               }
+            ForEach(items){ item in
+             self.getArcCell(item: item, arcCell: self.arcCell(item))
+            }
+    }
+    }
+    private func getArcView(item: Item) -> AnyView {
+        let shape = CircleArc(centerPoint: centerPoint,
+                              raduis: raduis,
+                              startAngle: Angle.init(radians:  Double(Double(item.index * angle).degreesToRadians)),
+                              endAngle: Angle.init(radians:  Double(Double((item.index + 1) * angle).degreesToRadians)),
+                              clockwise: false)
+        return AnyView( shape.stroke().foregroundColor(item.color).background(shape.foregroundColor(item.color)) )
+    }
+    private func getArcCell(item: Item, arcCell: ArcCell) -> some View {
+        let x = raduis * CGFloat(cos((Double(angle) * Double(item.index)) + shift))
+        let y = raduis * CGFloat(sin((Double(angle) * Double(item.index)) + shift))
+
+        return arcCell.position(CGPoint(x: x, y: y))
+      }
+    
+}
+struct CircleArc: Shape {
+    let centerPoint: CGPoint
+    let raduis: CGFloat
+    let startAngle: Angle
+    let endAngle: Angle
+    let clockwise: Bool
     func path(in rect: CGRect) -> Path {
         var path = Path()
-        path.addArc(center: CGPoint.init(x: rect.width * 0.25,
-                                         y: rect.width * 0.25),
-                    radius: rect.width * 0.25,
-                    startAngle: Angle(degrees: 0),
-                    endAngle: Angle(degrees: 180),
-                    clockwise: true)
+       
+        path.addArc(center: centerPoint,
+        radius: raduis,
+        startAngle: startAngle,
+        endAngle: endAngle,
+        clockwise: clockwise)
+        path.addLine(to: centerPoint)
+        path.closeSubpath()
         return path
     }
-    
-    
 }
-struct Circular: Shape {
-    let numofArcs: Int
-    func path(in rect: CGRect) -> Path {
-       var largePath = Path()
-        let raduis = CGFloat(rect.width / 2)
-        let angle = 360 / numofArcs
-        let centerX = rect.width / 2
-        let centerY = rect.height / 2
-        for arc in 0...numofArcs - 1 {
-            var path = Path()
-            let centerPoint = CGPoint(x: centerX,y: centerY)
-            path.addArc(center: centerPoint,
-            radius: raduis,
-            startAngle: Angle.init(radians:  Double(Double(arc * angle).degreesToRadians)),
-            endAngle: Angle.init(radians:  Double(Double((arc + 1) * angle).degreesToRadians)),
-            clockwise: false)
-            path.addLine(to: centerPoint)
-            path.closeSubpath()
-            
-            largePath.addPath(path)
-
-            }
-        return largePath
-        }
-    }
-
 extension Double {
     var degreesToRadians: CGFloat {
         get {
             return CGFloat((self * .pi) / 180)
         }
+    }
+}
+
+struct Item: Identifiable {
+    let id = UUID()
+    let index: Int
+    let name: String
+    let color: Color
+}
+
+struct ArcCell: View {
+    let name: String
+    var body: some View {
+        Text(name).foregroundColor(.pink)
     }
 }
